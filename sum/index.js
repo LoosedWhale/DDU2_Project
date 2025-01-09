b1368d056c07ce5dfabe04023b30fee95f0e6bbb
Theodor <81869656+LoosedWhale@users.noreply.github.com>
2025-01-09 15:39:53 +0100

    changed it back to 0

--- sum/index.js
+++ sum/index.js
@@ -41,7 +41,7 @@ reset.addEventListener("click", function () {
         div.classList.remove("marked");
     }
 
-    markedSum = "-";
+    markedSum = 0;
     document.getElementById("sumMarked").textContent = markedSum;
 });
 
